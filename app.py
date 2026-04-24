7c4c141f029e59520d6111b5e84dc9437b63aa23
Harry Patrice TEGUE KEMGNE <harry.tegue@eivp-paris.fr>
2026-04-24 23:27:52 +0200

    Fix import statement for RecursiveCharacterTextSplitter

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from pathlib import Path
 
 # LangChain
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
